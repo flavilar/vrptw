5a4caef08c24172573f24e6b0eea506480036dea
Macroover Ludvig <nest479@gmail.com>
2026-01-01 17:58:46 +0800

    Add simple 2-opt

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 import numpy as np
 from parser import read_solomon_instance
 from utils import calculate_euclidean_matrix
-from solver import GreedySolver
+from solver import GreedySolver, LocalSearch
 
 def main():
     print("VRPTW environment ready")
     print("NumPy version:", np.__version__)
-    instance = read_solomon_instance("../data/benchmarks/solomon-100/c101.txt")
+    instance = read_solomon_instance("../data/benchmarks/solomon-100/r102.txt")
     print(instance)
     distance = calculate_euclidean_matrix(instance.nodes)
     print(distance[0][1])
 
     greedy_solver = GreedySolver(instance)
     routes = greedy_solver.solve()
+    print(routes)
+
+    local_search = LocalSearch()
+    for route in routes:
+        local_search.optimize_route(route)
     print(routes)
 
 if __name__ == "__main__":
--- src/solver.py
+++ src/solver.py
@@ -98,3 +98,63 @@ class GreedySolver:
             current_route.close_route()
             self.routes.append(current_route)
         return self.routes
+
+
+class LocalSearch:
+    """
+    Implements 2-Opt.
+    """
+
+    def __init__(self):
+        self.dist_matrix = None
+
+    def optimize_route(self, route):
+        """
+        Applies 2-Opt to a single Route object to reduce its distance.
+        This modifies the 'route.sequence' in place.
+        """
+        self.dist_matrix = route.dist_matrix
+        improved = True
+        while improved:
+            improved = False
+            n = len(route.sequence)
+
+            for i in range(1, n - 2):
+                for j in range(i + 1, n - 1):
+                    # Current edges: (i-1 -> i) and (j -> j+1)
+                    # Candidate edges: (i-1 -> j) and (i -> j+1)
+                    # Reverses the segment between i and j
+
+                    if self._calculate_2opt_gain(route.sequence, i, j) < 0:
+                        # If gain is negative, it means distance decreases
+                        self._apply_2opt_swap(route.sequence, i, j)
+                        route.cost = self._recalculate_cost(route.sequence)
+                        improved = True
+
+            # TODO: In a full VRPTW implementation, is_feasible() must be checked here.
+
+    def _calculate_2opt_gain(self, sequence, i, j):
+        """
+        Calculates the change in distance if we swap.
+        """
+        a = sequence[i - 1]
+        b = sequence[i]
+        c = sequence[j]
+        d = sequence[j + 1]
+
+        d_current = self.dist_matrix[a][b] + self.dist_matrix[c][d]
+        d_new = self.dist_matrix[a][c] + self.dist_matrix[b][d]
+
+        return d_new - d_current
+
+    def _apply_2opt_swap(self, sequence, i, j):
+        """
+        Reverses the sub-segment of the list.
+        """
+        sequence[i:j + 1] = reversed(sequence[i:j + 1])
+
+    def _recalculate_cost(self, sequence):
+        cost = 0.0
+        for k in range(len(sequence) - 1):
+            cost += self.dist_matrix[sequence[k]][sequence[k + 1]]
+        return cost
